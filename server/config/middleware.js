f4cd4f7484a9b35153857058f0490f424d163f45
Dianna Faulk <diannafaulk@gmail.com>
2015-09-24 20:14:01 -0700

    (routing) Create express ImageRouter, forward /api/images requests

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,10 +1,13 @@
 var morgan = require('morgan');
 
 module.exports = function(app, express){
+  var imageRouter = express.Router();
+
   app.use(morgan('dev'));
+
   app.use(express.static(__dirname+'/../../client/'));
   app.use('/bower_components', express.static(__dirname+'/../../bower_components/'))
-
+  app.use('/api/images', imageRouter);
 
   // app.use('/app', express.static(__dirname+'/client/app/'));
   // app.use('/gallery', express.static(__dirname+'/client/app/gallery/'));
@@ -12,4 +15,6 @@ module.exports = function(app, express){
   app.get('/', function(req, res){
     res.send('index.html');
   });
+
+  require('./images/imageRoutes.js')(imageRouter);
 };
