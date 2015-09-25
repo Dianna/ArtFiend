bba3319b493bea8ce469bc4df98d8caff6caa9f6
Dianna Faulk <diannafaulk@gmail.com>
2015-09-24 19:52:42 -0700

    (dependencies) Use local mongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+mongoose.connect('mongodb://localhost/artFiend');
 
 require('./config/middleware.js')(app, express);
 
