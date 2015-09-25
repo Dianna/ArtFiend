2de6599eff1c442ba150b7d235e96db71a3a1389
Dianna Faulk <diannafaulk@gmail.com>
2015-09-24 20:12:31 -0700

    (gallery) Call getImages from image Services

--- client/app/gallery/gallery.js
+++ client/app/gallery/gallery.js
@@ -1,9 +1,16 @@
 angular.module('artFiend.gallery', [])
-.controller('GalleryCtrl', function($scope){
+.controller('GalleryCtrl', function($scope, Images){
   $scope.images = [];
   $scope.getImages = function(){
     console.log('getting images');
-    $scope.images.push('in scope images');
+    Images.getImages()
+      .then(function(data){
+        console.log('data in GalleryCtrl: ', data);
+        $scope.images = data;
+      })
+      .catch(function(err){
+        console.error(err);
+      });
     console.log($scope.images);
   }
   $scope.getImages();
