00e49de4972ba0569ab00493278da1c936be8472
Dianna Faulk <diannafaulk@gmail.com>
2015-10-07 18:06:54 -0700

    (db) log errors

--- server/server.js
+++ server/server.js
@@ -2,8 +2,15 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
-var db = mongoose.connect('mongodb://localhost/artFiend');
+
 
 require('./config/middleware.js')(app, express);
+
+var mongoURI = "mongodb://localhost:27017/artFiend";
+var db = mongoose.connect(mongoURI).connection;
+db.on('error', function(err) { console.log(err.message); });
+db.once('open', function() {
+  console.log("mongodb connection open");
+});
 
 module.exports = app;
